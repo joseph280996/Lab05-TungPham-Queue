39668455da0ff66a427cf2864fc8b969c792fcd0
joseph280996 <joseph.pham280996@gmail.com>
2017-11-07 10:51:54 -0500

    Update the overloading operator

--- lab05/Queue.cpp
+++ lab05/Queue.cpp
@@ -50,27 +50,9 @@ int Queue::dequeue(char* read_val) {
 }
 int Queue::operator += (char value)
 {
-	if (count<arr_size) //if array is not full, execute below
-	{
-		arr[tail++] = value;  //pass value of first-to-last element in array
-		count++; //counting the input value
-		if (tail == arr_size) tail = 0; //rewind the index back to the first if it reach the size input
-		return 0;
-	}
-	else {
-		return -1;
-	}
+	return enqueue(value);
 }
 int Queue::operator -= (char& read_val)
 {
-	if (count >0) { //if array has elements, execute below
-		read_val = arr[head++]; //pass-by-reference the value of first-to-last element in array to parameter of function
-		arr[head - 1] = NULL; //reassign the dequeue value back to NULL
-		count--; //decrement the size of the queue
-		if (head == arr_size) head = 0;//rewind the index back to the first if it reach the size input
-		return 0;
-	}
-	else { //If array is empty, assign the dequeue index to the enqueue index
-		return -1;
-	}
+	return dequeue(read_val);
 }
